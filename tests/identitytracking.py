728a1ca7394e75cccf1f9f465788ea7a3774afe1
tphanson <tphanson@kambria.io>
2019-12-10 17:10:03 +0700

    improce model

--- tests/identitytracking.py
+++ tests/identitytracking.py
@@ -6,17 +6,21 @@ from src.identitytracking import IdentityTracking
 from src.datamanufacture import DataManufacture
 from src.humandetection import HumanDetection
 
-VIDEO3 = os.path.join(os.path.dirname(
-    os.path.abspath(__file__)), "../data/video/MOT17-03-SDP.mp4")
+VIDEO0 = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "../data/video/3rd-camera.mp4")
 VIDEO5 = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "../data/video/MOT17-05-SDP.mp4")
+VIDEO6 = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "../data/video/MOT17-06-SDP.mp4")
+VIDEO9 = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), "../data/video/MOT17-09-FRCNN.mp4")
 
 
 def train():
     idtr = IdentityTracking()
-    # names = ['MOT17-05']
-    names = ['MOT17-02', 'MOT17-04', 'MOT17-05',
-             'MOT17-09', 'MOT17-10', 'MOT17-11', 'MOT17-13']
+    names = ['MOT17-05']
+    # names = ['MOT17-02', 'MOT17-04', 'MOT17-05',
+    #          'MOT17-09', 'MOT17-10', 'MOT17-11', 'MOT17-13']
 
     pipeline = None
     for name in names:
@@ -36,7 +40,7 @@ def predict():
     idtr = IdentityTracking()
     hd = HumanDetection()
 
-    cap = cv.VideoCapture(VIDEO3)
+    cap = cv.VideoCapture(VIDEO9)
     if (cap.isOpened() == False):
         print("Error opening video stream or file")
 
@@ -53,8 +57,10 @@ def predict():
         objs = hd.predict(img)
 
         if is_first_frames > 0:
-            is_first_frames -= 1
-            histories.append((objs[1], img))
+            index = 0
+            if len(objs) > index:
+                is_first_frames -= 1
+                histories.append((objs[index], img))
         else:
             inputs = []
             for obj in objs:
@@ -62,17 +68,21 @@ def predict():
                 tensor.pop(0)
                 tensor.append((obj, img))
                 inputs.append(tensor)
-            predictions, argmax = idtr.predict(inputs)
-            predictions = predictions.numpy()
-            argmax = argmax.numpy()
-            if predictions[argmax] >= 0.2:
-                obj = objs[argmax]
-                histories.pop(0)
-                histories.append((obj, img))
-                image.draw_box(img, [obj])
+            if len(inputs) > 0:
+                predictions, argmax = idtr.predict(inputs)
+                predictions = predictions.numpy()
+                argmax = argmax.numpy()
+                if predictions[argmax] >= 0.2:
+                    obj = objs[argmax]
+                    histories.pop(0)
+                    histories.append((obj, img))
+                    image.draw_box(img, [obj])
 
-            print("==================")
-            print(predictions)
+                print("==================")
+                print(predictions)
+
+        # Test human detection
+        # image.draw_box(img, objs)
 
         img = image.convert_pil_to_cv(img)
         cv.imshow('Video', img)
