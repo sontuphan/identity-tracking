dc32875b23bdfe348d179d884394a06190a6d912
tphanson <tphanson@kambria.io>
2019-12-17 10:30:09 +0700

    fix data

--- tests/identitytracking.py
+++ tests/identitytracking.py
@@ -3,7 +3,7 @@ import cv2 as cv
 import numpy as np
 
 from utils import image
-from src.humandetection import HumanDetection
+# from src.humandetection import HumanDetection
 from src.identitytracking import IdentityTracking
 from src.datamanufacture import DataManufacture
 
@@ -37,68 +37,68 @@ def train():
     idtr.train(dataset, 10)
 
 
-def predict():
-    idtr = IdentityTracking()
-    hd = HumanDetection()
+# def predict():
+#     idtr = IdentityTracking()
+#     hd = HumanDetection()
 
-    cap = cv.VideoCapture(VIDEO9)
-    if (cap.isOpened() == False):
-        print("Error opening video stream or file")
+#     cap = cv.VideoCapture(VIDEO9)
+#     if (cap.isOpened() == False):
+#         print("Error opening video stream or file")
 
-    is_first_frames = True
-    histories = []
-    while(cap.isOpened()):
-        ret, frame = cap.read()
+#     is_first_frames = True
+#     histories = []
+#     while(cap.isOpened()):
+#         ret, frame = cap.read()
 
-        if ret != True:
-            break
+#         if ret != True:
+#             break
 
-        img = image.convert_cv_to_pil(frame)
-        img = image.resize(img, (640, 480))
-        objs = hd.predict(img)
+#         img = image.convert_cv_to_pil(frame)
+#         img = image.resize(img, (640, 480))
+#         objs = hd.predict(img)
 
-        if is_first_frames:
-            is_first_frames = False
-            for _ in range(idtr.tensor_length):
-                histories.append((objs[0], img))
-        else:
-            inputs = []
-            for obj in objs:
-                tensor = histories.copy()
-                tensor.pop(0)
-                tensor.append((obj, img))
-                inputs.append(tensor)
-            if len(inputs) > 0:
-                predictions, argmax = idtr.predict(inputs)
-                predictions = predictions.numpy()
-                argmax = argmax.numpy()
-                if predictions[argmax] >= 0.4:
-                    obj = objs[argmax]
-                    histories.pop(0)
-                    histories.append((obj, img.copy()))
-                    image.draw_box(img, [obj])
+#         if is_first_frames:
+#             is_first_frames = False
+#             for _ in range(idtr.tensor_length):
+#                 histories.append((objs[0], img))
+#         else:
+#             inputs = []
+#             for obj in objs:
+#                 tensor = histories.copy()
+#                 tensor.pop(0)
+#                 tensor.append((obj, img))
+#                 inputs.append(tensor)
+#             if len(inputs) > 0:
+#                 predictions, argmax = idtr.predict(inputs)
+#                 predictions = predictions.numpy()
+#                 argmax = argmax.numpy()
+#                 if predictions[argmax] >= 0.4:
+#                     obj = objs[argmax]
+#                     histories.pop(0)
+#                     histories.append((obj, img.copy()))
+#                     image.draw_box(img, [obj])
 
-                print("==================")
-                print(predictions)
+#                 print("==================")
+#                 print(predictions)
 
-        # Test human detection
-        # image.draw_box(img, objs)
-        # Test historical frames
-        his_img = None
-        for history in histories:
-            (_obj, _img) = history
-            if his_img is None:
-                cropped_img = image.crop(_img, _obj)
-                resized_img = image.resize(cropped_img, (96, 96))
-                his_img = image.convert_pil_to_cv(resized_img)
-            else:
-                cropped_img = image.crop(_img, _obj)
-                resized_img = image.resize(cropped_img, (96, 96))
-                his_img = np.concatenate(
-                    (his_img, image.convert_pil_to_cv(resized_img)), axis=1)
-        cv.imshow('History', his_img)
+#         # Test human detection
+#         # image.draw_box(img, objs)
+#         # Test historical frames
+#         his_img = None
+#         for history in histories:
+#             (_obj, _img) = history
+#             if his_img is None:
+#                 cropped_img = image.crop(_img, _obj)
+#                 resized_img = image.resize(cropped_img, (96, 96))
+#                 his_img = image.convert_pil_to_cv(resized_img)
+#             else:
+#                 cropped_img = image.crop(_img, _obj)
+#                 resized_img = image.resize(cropped_img, (96, 96))
+#                 his_img = np.concatenate(
+#                     (his_img, image.convert_pil_to_cv(resized_img)), axis=1)
+#         cv.imshow('History', his_img)
 
-        img = image.convert_pil_to_cv(img)
-        cv.imshow('Video', img)
-        if cv.waitKey(10) & 0xFF == ord('q'):
-            break
+#         img = image.convert_pil_to_cv(img)
+#         cv.imshow('Video', img)
+#         if cv.waitKey(10) & 0xFF == ord('q'):
+#             break
